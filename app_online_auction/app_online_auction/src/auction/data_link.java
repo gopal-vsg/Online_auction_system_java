63863651c6e3a5eb2bf2918c1779d12bc32efcc9
gopal-vsg <119056598+gopal-vsg@users.noreply.github.com>
2023-10-16 23:57:56 +0530

    Update data_link.java

--- app_online_auction/app_online_auction/src/auction/data_link.java
+++ app_online_auction/app_online_auction/src/auction/data_link.java
@@ -1,12 +1,21 @@
 package auction;
-import java.sql.*;
-import java.util.Scanner;
-import java.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.event.AncestorListener;
 
-public class data_link {
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class data_link implements ActionListener {
     private Connection con;
-    private Scanner input;
-
+    Scanner input = new Scanner(System.in);
+    public Connection connection;
     public data_link() {
         String url = "jdbc:mysql://localhost:3306/app_users";
         String username = "root";
@@ -16,43 +25,84 @@ public class data_link {
             System.out.println("The connection is established!!");
         } catch (SQLException e) {
             e.printStackTrace();
-            System.exit(1); // Exit the program on connection error
+            System.exit(1); 
         }
-        input = new Scanner(System.in); // Initialize the Scanner
+        
     }
     
-    public void sign_up() {
-        try {
-            System.out.println("Enter a unique user_id: ");
-            String id = input.nextLine();
+ 
 
-            System.out.println("Enter your name: ");
-            String name = input.nextLine();
+public class SignUpGUI implements ActionListener{
+    public SignUpGUI(Object object) {
+    }
+    public AncestorListener usersign() {
+        JFrame signwin;
+    JTextField userIdField, nameField, mobileField, addressField, emailField;
+     JPasswordField passwordField;
+     JComboBox<String> userTypeComboBox;
+        signwin = new JFrame("Sign Up");
+        signwin.setLayout(new GridLayout(8, 2));
 
-            System.out.println("Enter the password: ");
-            String userPassword = input.nextLine();
+        signwin.add(new JLabel("Enter User ID: "));
+        userIdField = new JTextField();
+        signwin.add(userIdField);
 
-            System.out.println("Enter your mobile number: ");
-            int mobile = Integer.parseInt(input.nextLine());
+        signwin.add(new JLabel("Enter Your Name: "));
+        nameField = new JTextField();
+        signwin.add(nameField);
 
-            System.out.println("Enter your address: ");
-            String address = input.nextLine();
+        signwin.add(new JLabel("Enter Password: "));
+        passwordField = new JPasswordField();
+        signwin.add(passwordField);
 
-            System.out.println("Enter your email: ");
-            String email = input.nextLine();
+        signwin.add(new JLabel("Enter Mobile Number: "));
+        mobileField = new JTextField();
+        signwin.add(mobileField);
 
-            System.out.println("What type of user: buyer or seller? ");
-            String type = input.nextLine().toLowerCase();
+        signwin.add(new JLabel("Enter Address: "));
+        addressField = new JTextField();
+        signwin.add(addressField);
 
-            String user_type;
+        signwin.add(new JLabel("Enter Email: "));
+        emailField = new JTextField();
+        signwin.add(emailField);
 
-            if (type.equals("buyer") || type.equals("seller")) {
-                user_type = type;
-            } else {
-                System.out.println("Please enter 'buyer' or 'seller'");
-                return; // Exit the sign-up process if user type is invalid
+        signwin.add(new JLabel("Select User Type: "));
+        String[] userTypes = {"buyer", "seller"};
+        userTypeComboBox = new JComboBox<>(userTypes);
+        signwin.add(userTypeComboBox);
+
+        JButton signUpButton = new JButton("Sign Up");
+        signUpButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Gather user input from GUI components
+                String id = userIdField.getText();
+                String name = nameField.getText();
+                String userPassword = new String(passwordField.getPassword());
+                int mobile = Integer.parseInt(mobileField.getText());
+                String address = addressField.getText();
+                String email = emailField.getText();
+                String userType = (String) userTypeComboBox.getSelectedItem();
+
+                // Call your sign_up method with the input values
+                try {
+                    sign_up(id, name, userPassword, mobile, address, email, userType);
+                } catch (SQLException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
             }
+        });
+        signwin.add(signUpButton);
 
+        signwin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        signwin.setSize(400, 300);
+        signwin.setVisible(true);
+        return null;
+    }
+    public void sign_up(String id, String name, String userPassword, int mobile, String address, String email, String userType) throws SQLException{
+        
             String query = "INSERT INTO user_details (user_id, user_name, password, phone_num, address, email, user_type) VALUES (?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement pstm = con.prepareStatement(query);
 
@@ -62,7 +112,7 @@ public class data_link {
             pstm.setInt(4, mobile);
             pstm.setString(5, address);
             pstm.setString(6, email);
-            pstm.setString(7, user_type);
+            pstm.setString(7, userType);
 
             int rowsAffected = pstm.executeUpdate();
 
@@ -73,39 +123,69 @@ public class data_link {
             }
 
             pstm.close();
-        } catch (SQLException | NumberFormatException e) {
-            e.printStackTrace();
-        }
+        
+   
     }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+}
+public void login() {
+        JFrame loginFrame = new JFrame("Login");
+        loginFrame.setLayout(new GridLayout(3, 2));
 
-    public void login() {
-        try {
-            System.out.println("Enter your user_id: ");
-            String id = input.nextLine();
+        JTextField userIdField = new JTextField();
+        JPasswordField passwordField = new JPasswordField();
 
-            System.out.println("Enter your password: ");
-            String password = input.nextLine();
+        loginFrame.add(new JLabel("Enter User ID: "));
+        loginFrame.add(userIdField);
 
-            String query = "SELECT * FROM user_details WHERE user_id = ? AND password = ?";
-            PreparedStatement pstm = con.prepareStatement(query);
+        loginFrame.add(new JLabel("Enter Password: "));
+        loginFrame.add(passwordField);
 
-            pstm.setString(1, id);
-            pstm.setString(2, password);
+        JButton loginButton = new JButton("Login");
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String id = userIdField.getText();
+                String password = new String(passwordField.getPassword());
 
-            ResultSet rs = pstm.executeQuery();
+                try {
+                    // Perform login verification
+                    String query = "SELECT * FROM user_details WHERE user_id = ? AND password = ?";
+                    PreparedStatement pstm = con.prepareStatement(query);
+                    pstm.setString(1, id);
+                    pstm.setString(2, password);
+                    ResultSet rs = pstm.executeQuery();
 
-            if (rs.next()) {
-                System.out.println("Login successful!");
-                // You can add code here to perform actions after successful login
-            } else {
-                System.out.println("Invalid user_id or password.");
+                    if (rs.next()) {
+                        JOptionPane.showMessageDialog(null, "Login successful!");
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Invalid user_id or password.");
+                    }
+
+                    rs.close();
+                    pstm.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
             }
+        });
 
-            rs.close();
-            pstm.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        loginFrame.add(loginButton);
+
+        loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        loginFrame.setSize(300, 150);
+        loginFrame.setVisible(true);
     }
+@Override
+public void actionPerformed(ActionEvent e) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+}
 
 }
+    
+ 
